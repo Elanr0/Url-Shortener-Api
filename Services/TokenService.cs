8dc3cec161fe96cc5f173149c50d6189d9e63458
Elanur Karaca <elanur.karaca@FAIRTEKNOLOJI.LOCAL>
2026-06-04 09:25:46 +0300

    Add token service skeleton

---
+++ Services/TokenService.cs
@@ -0,0 +1,12 @@
+using UrlShortener.Models;
+
+namespace UrlShortener.Services;
+
+public class TokenService
+{
+    public string CreateToken(User user) 
+    {
+        throw new NotImplementedException();
+    }
+
+}
